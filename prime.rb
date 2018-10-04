b1946227d44d8f4335f960e837a8fe100a2dc0eb
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-10-04 17:15:46 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,8 +5,10 @@ require 'bigdecimal/math'
 def prime?(number)
   #returns true for prime numbers
   #returns false for non-prime numbers
-  divisor = [2, 3, 5, 7, 11]
-  
+  divisorlist = [2, 3, 5, 7, 11]
+  divisorlist.any? do |divisor|
+    number % divisor != 0
+  end
 end
 
 # calculate pi to 10k digits
